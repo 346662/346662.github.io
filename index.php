3f65e3f4376358b9a137fe0fc3a05a0867f23a00
346662 <61870913+346662@users.noreply.github.com>
2020-03-06 21:06:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,355 +1,162 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]--><head>
-<?php include("../plugin/config2.php"); ?>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>简约星空主页_个人网页制作_<?php echo htmlspecialchars(urldecode($title)); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="<?php echo htmlspecialchars(urldecode($title)); ?>是一款全自动在线网页制作工具，使用144G核心技术，国内顶尖自动化网页制作系统，只需选择模板一键生成即可。可制作一系列个人网页、表白网页、HTML5网页等等，人人都能做网页。" />
-<meta name="keywords" content="在线网页制作,个人网页制作,表白网页制作,祝福网页制作,免费网页制作,网页一键生成" />
-<!-- Animate.css -->
-<link rel="stylesheet" href="../css/animate.css">
-<!-- Flexslider -->
-<link rel="stylesheet" href="../css/flexslider.css">
-<!-- Icomoon Icon Fonts-->
-<link rel="stylesheet" href="../css/icomoon.css">
-<!-- Magnific Popup -->
-<link rel="stylesheet" href="../css/magnific-popup.css">
-<!-- Bootstrap  -->
-<link rel="stylesheet" href="../css/bootstrap.css">
-<!-- Bootstrap Date 
-<link href="http://cdn.bootcss.com/twitter-bootstrap/2.2.2/css/bootstrap.min.css" rel="stylesheet"> -->
-<link rel="stylesheet" type="text/css" href="../css/bootstrap-datetimepicker.css">
-<link rel="stylesheet" href="../css/style.css">
-<!-- Styleswitcher ( This style is for demo purposes only, you may delete this anytime. ) -->
-<link rel="stylesheet" id="theme-switch" href="../css/style.css">
-<!-- End demo purposes only -->
-<!-- Modernizr JS -->
-<script src="../js/modernizr-2.6.2.min.js"></script>
+<!DOCTYPE HTML>
+<html>
+<head>
+<base href="<?php echo $baseurl?>geren_jianyuexingkong/" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo $con1?></title>
+<link rel="Bookmark" href="favicon.ico" />
+<link href="css/index_style.css" rel="stylesheet" type="text/css">
+<script src="js/jquery.min.js" type="text/javascript"></script>
+
 </head>
-
 <body>
-<!-- Loader -->
-<div class="fh5co-loader"></div>
-<!--Baidu display logo-->
-<div class="dnone"><img width="121" src="logo.png" alt="logo" /></div>
-<!--Baidu display logo END-->
-<div id="fh5co-page">
-  <section id="fh5co-header">
-    <div class="container">
-      <nav role="navigation">
-        <ul class="pull-left left-menu">
-          <li><a href="../geren.php">个人网页</a></li>
-          <li><a href="../biaobai.php">表白网页</a></li>
-          <li><a href="../quwei.php">趣味网页</a></li>
-        </ul>
-        <h1 id="fh5co-logo"><a href="../"><?php echo htmlspecialchars(urldecode($logo)); ?><span>.</span></a></h1>
-        <ul class="pull-right right-menu">
-          <li><a href="<?php if(!preg_match("/^(http|https):/", htmlspecialchars(urldecode($right_top_url_1)))) echo '../'; echo htmlspecialchars(urldecode($right_top_url_1)); ?>"><?php echo htmlspecialchars(urldecode($right_top_title_1)); ?></a></li>
-          <li class="fh5co-cta-btn"><a id="login" href="#" data-toggle="modal" data-target="#myModal">贵宾登录</a></li>
-        </ul>
-        <ul class="zhong dnone">
-          <li><a href="../">返回首页</a></li>
-        </ul>
-      </nav>
-    </div>
-  </section>
-  <!-- #fh5co-header -->
-  <section id="fh5co-hero" class="no-js-fullheight" style="background-image: url(../images/nav-geren.jpg);" data-next="yes">
-    <div class="fh5co-overlay"></div>
-    <div class="container">
-      <div class="fh5co-intro no-js-fullheight">
-        <div class="fh5co-intro-text">
-          <div class="fh5co-center-position">
-            <h2 class="animate-box">简约星空主页</h2>
-            <h3 class="animate-box"><a href="../web.php?id=RXThJAI" style="color:#FFF" target="_blank">点击预览</a></h3>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="fh5co-learn-more animate-box"> <a href="#" class="scroll-btn"> <span class="text">填入网页信息</span> <span class="arrow"><i class="icon-chevron-down"></i></span> </a> </div>
-  </section>
-  <!-- END #fh5co-hero -->
-  <section id="fh5co-projects">
-    <div class="container animate-box">
-    <div class="row">
-    <form method="post" action="../result.php">
-      <div class="col-md-12">
-        <div class="form-group">
-          <input type="button" class="btn btn-outline btn-block" onclick="shuru()" value="使用默认文字">
-        </div>
-      </div>
-      <div class="col-md-12">
-        <div class="form-group">
-          <input id="con1" name="con1" type="text" class="form-control" placeholder="请输入网页标题">
-        </div>
-      </div>
-	  <div class="col-md-12">
-        <div class="form-group">
-          <input id="con2" name="con2" type="text" class="form-control" placeholder="请输入首页名称">
-        </div>
-      </div>
-      <div class="col-md-12">
-        <div class="form-group">
-          <input id="con3" name="con3" type="text" class="form-control" placeholder="请输入首页链接">
-        </div>
-      </div>
-	  <div class="col-md-12">
-        <div class="form-group">
-          <input id="con4" name="con4" type="text" class="form-control" placeholder="请输您的ＱＱ号">
-        </div>
-      </div>
-	  <div class="col-md-12">
-        <div class="form-group">
-          <textarea id="con5" name="con5" type="text" class="form-control textarea" rows="5" placeholder="请输入提示公告"></textarea>
-        </div>
-      </div>
-      <div class="col-md-12">
-        <div class="form-group">
-          <div id="fh5co-subscribe">
-            <input id="con6" name="con6" type="text" class="form-control" placeholder="请输入背景音乐URL地址">
-            <input type="button" value="获取音乐" class="btn btn-primary" onclick="window.open('../yinyue.php','_blank')">
-          </div>
-        </div>
-      </div>
-      <div class="col-md-12">
-          <div class="form-group">
-            <input type="submit" class="btn btn-primary btn-lg btn-block" value="立即制作">
-          </div>
-      </div>
-      </form>
-    </div>
-  </section>
-	<script>
-        function shuru(){
-            var con1=document.getElementById('con1');
-            var con2=document.getElementById('con2');
-            var con3=document.getElementById('con3');
-            var con4=document.getElementById('con4');
-			var con5=document.getElementById('con5');
-            con1.value='144G网页制作';
-			con2.value='简约星空主页';
-            con3.value='http://www.144g.com/';
-            con4.value='123456';
-            con5.value='144G是一款全自动在线网页制作工具，国内顶尖自动化网页制作系统，只需选择模板一键生成即可。可制作一系列个人网页、表白网页、HTML5网页等等，人人都能做网页。';
-			con6.value='http://p2.music.126.net/pOH2QXwQc_7IX4l2sB-iSA==/7954966629620463.mp3';
-        }
-    </script> 
-  <!-- END #fh5co-projects -->
-  <div id="fh5co-info">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h2 class="fh5co-lead animate-box"><?php echo htmlspecialchars(urldecode($tsbt)); ?></h2>
-          <p class="fh5co-sub-lead animate-box"><?php echo htmlspecialchars(urldecode($tstext1)); ?></p>
-          <p class="fh5co-sub-lead animate-box"><?php echo htmlspecialchars(urldecode($tstext2)); ?></p>
-          <p class="fh5co-sub-lead animate-box"><?php echo htmlspecialchars(urldecode($tstext3)); ?></p>
-          <p class="fh5co-sub-lead animate-box"><?php echo htmlspecialchars(urldecode($tstext4)); ?></p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- END #fh5co-info -->
-  <footer id="fh5co-footer">
-    <div class="container">
-      <div class="row row-bottom-padded-md">
-        <div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-          <div class="fh5co-footer-widget">
-            <h3>模板分类</h3>
-            <ul class="fh5co-links">
-              <li><a href="../geren.php" target="_blank">个人网页制作</a></li> 
-              <li><a href="../biaobai.php" target="_blank">表白网页制作</a></li>
-              <li><a href="../quwei.php" target="_blank">趣味网页制作</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-          <div class="fh5co-footer-widget">
-            <h3>技术支持</h3>
-            <ul class="fh5co-links">
-              <li><a href="../tupian.php" target="_blank">图片外链</a></li>
-              <li><a href="../yinyue.php" target="_blank">音乐外链</a></li>
-              <li><a href="../liuyan.php" target="_blank">留言反馈</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-          <div class="fh5co-footer-widget">
-            <h3>关于我们</h3>
-            <p>客服QQ：<a href="http://wpa.qq.com/msgrd?v=3&uin=<?php echo htmlspecialchars(urldecode($qq)); ?>&site=qq&menu=yes" target="_blank" title="QQ号：<?php echo htmlspecialchars(urldecode($qq)); ?>"><?php echo htmlspecialchars(urldecode($qq)); ?></a><br><br>
-            E-mail：<a href="mailto:<?php echo htmlspecialchars(urldecode($email)); ?>"><?php echo htmlspecialchars(urldecode($email)); ?></a>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-          <div class="fh5co-footer-widget">
-            <h3>二维码</h3>
-            <div class="col-md-8 col-sm-6 col-xs-4">
-                <img src="<?php if(!preg_match("/^(http|https):/", htmlspecialchars(urldecode($footewm)))) echo '../'; echo htmlspecialchars(urldecode($footewm)); ?>" class="img-thumbnail img-responsive">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="fh5co-copyright animate-box">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <p class="fh5co-left"><small>&copy; 2017 <a href="./" target="_blank"><?php echo htmlspecialchars(urldecode($title)); ?></a> Corporation.&nbsp;<?php echo urldecode($tonji); ?></small></p>
-            <p class="fh5co-right"><small class="fh5co-right"><a><?php echo htmlspecialchars(urldecode($right_foot)); ?></a></small></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- END #fh5co-footer --> 
+
+
+
+<div class="header" id="demo">
+
+	<div class="topcn" style="font:30px/15px 'microsoft yahei'; color:#0FF;text-align:center;">
+	  <p>&nbsp;</p>
+	  <p><?php echo $con2?></p>
+	</div>
+
+    <script type="text/javascript">
+    function duihua()
+	{
+	alert("<?php echo $con5?>")
+	}
+    </script>
+    <div class="nav">
+		 <a class="gv" href="<http://dianxin.kahao114.com/>" align="center" target="_blank">中泽卡充值1</a>
+         <a class="gv" href="http://xiaozhi.kahao114.com/>" align="center" target="_blank">中泽卡充值2</a>
+		 <a class="gv" href="https://iot.ctboss.net/user/login>" onclick="duihua()" align="center" target="_blank">移动卡充值</a>
+		 <a class="gv" href="http://zy.iot688.com/iot/iotCeslogin.html" align="center" target="_blank">大龙卡充值</a>
+   </div>
+    
+  <div class="canvaszz"> </div>
+  <canvas id="canvas"></canvas> 
 </div>
-<!-- END #fh5co-page --> 
-
-<!-- End demo purposes only --> 
-<!-- VIP membership system --> 
-<script src="../js/vip.js"></script>
-<!-- jQuery --> 
-<script src="../js/jquery.min.js"></script> 
-<!-- Bootstrap Date  -->
-<script src="../js/bootstrap-datetimepicker.js"></script> 
-<script src="../js/bootstrap-datetimepicker.zh-CN.js"></script> 
-<script type="text/javascript">
- $(".form_datetime").datetimepicker({
-      　minView: "month", //选择日期后，不会再跳转去选择时分秒 
-    　　format: "yyyy-mm-dd", //选择日期后，文本框显示的日期格式 
-    　　language: 'zh-CN', //汉化 
-    　　autoclose:true //选择日期后自动关闭 
-    })
-</script> 
-
-<!-- jQuery Easing --> 
-<script src="../js/jquery.easing.1.3.js"></script> 
-<!-- Bootstrap --> 
-<script src="../js/bootstrap.min.js"></script> 
-<!-- Waypoints --> 
-<script src="../js/jquery.waypoints.min.js"></script> 
-<!-- Flexslider --> 
-<script src="../js/jquery.flexslider-min.js"></script> 
-<!-- Magnific Popup --> 
-<script src="../js/jquery.magnific-popup.min.js"></script> 
-<script src="../js/magnific-popup-options.js"></script> 
-
-<!-- For demo purposes only styleswitcher ( You may delete this anytime ) --> 
-<script src="../js/jquery.style.switcher.js"></script> 
 
 
-<!-- Main JS (Do not remove) --> 
-<script src="../js/main.js"></script> 
+<!-----HEADER END----->
 
-<!-- 
-	INFO:
-	jQuery Cookie for Demo Purposes Only. 
-	The code below is to toggle the layout to boxed or wide 
-	--> 
-<script src="../js/jquery.cookie.js"></script> 
+<!--用来解决视频右键菜单，用于视频上面的遮罩层 START-->
+<div class="videozz"></div>
+<!--用来解决视频右键菜单，用于视频上面的遮罩层 END-->
+
+<!--音乐 END-->
+
+
 <script>
-		$(function(){
+//宇宙特效
+"use strict";
+var canvas = document.getElementById('canvas'),
+  ctx = canvas.getContext('2d'),
+  w = canvas.width = window.innerWidth,
+  h = canvas.height = window.innerHeight,
 
-			if ( $.cookie('layoutCookie') != '' ) {
-				$('body').addClass($.cookie('layoutCookie'));
-			}
+  hue = 217,
+  stars = [],
+  count = 0,
+  maxStars = 1300;//星星数量
 
-			$('a[data-layout="boxed"]').click(function(event){
-				event.preventDefault();
-				$.cookie('layoutCookie', 'boxed', { expires: 7, path: '/'});
-				$('body').addClass($.cookie('layoutCookie')); // the value is boxed.
-			});
+var canvas2 = document.createElement('canvas'),
+  ctx2 = canvas2.getContext('2d');
+canvas2.width = 100;
+canvas2.height = 100;
+var half = canvas2.width / 2,
+  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+gradient2.addColorStop(0.025, '#CCC');
+gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+gradient2.addColorStop(1, 'transparent');
 
-			$('a[data-layout="wide"]').click(function(event){
-				event.preventDefault();
-				$('body').removeClass($.cookie('layoutCookie')); // the value is boxed.
-				$.removeCookie('layoutCookie', { path: '/' }); // remove the value of our cookie 'layoutCookie'
-			});
-		});
-	</script>
-<!-- 模态框（Modal） -->
-  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> 
-   <div class="modal-dialog"> 
-    <div class="modal-content logintc"> 
-     <ul id="myTab" class="nav nav-tabs"> 
-      <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button> 
-      <li class="active"> <a href="#gblogin" data-toggle="tab"><span class="glyphicon glyphicon-user"></span>贵宾登录</a> </li> 
-      <li><a href="#update" data-toggle="tab"><span class="glyphicon glyphicon-cog"></span>修改密码</a></li> 
-     </ul> 
-     <div id="myTabContent" class="tab-content"> 
-      <div class="tab-pane fade in active" id="gblogin"> 
-       <div class="modal-body"> 
-        <div class="row"> 
-         <form method="post" action="../plugin/vip.php"> 
-          <input type="hidden" name="login" value="1" /> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input id="username" name="username" type="text" class="form-control" maxlength="20" placeholder="请输入登录账号" required /> 
-           </div> 
-          </div> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input id="password" name="password" type="password" class="form-control" placeholder="请输入登录密码" maxlength="30" required /> 
-           </div> 
-          </div> 
-		  <div class="col-md-12"> 
-           <div class="form-group"> 
-			<label class="baoliu">
-			<input type="checkbox" name="loginbl">登入保留一星期 
-			</label>
-           </div> 
-          </div> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input type="button" class="btn btn-default" onclick="window.open('../zhuce.php','_blank')" value="注册账户" /> 
-            <input type="submit" class="btn btn-primary" value="立即登录" /> 
-           </div> 
-          </div> 
-         </form> 
-        </div> 
-       </div> 
-      </div> 
-      <div class="tab-pane fade" id="update"> 
-       <div class="modal-body"> 
-        <div class="row"> 
-         <form method="post" action="../plugin/vip.php"> 
-          <input type="hidden" name="update" value="1" /> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input id="username" name="username" type="text" class="form-control" maxlength="20" placeholder="请输入账号" required /> 
-           </div> 
-          </div> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input id="password" name="password" type="password" class="form-control" placeholder="请输入旧密码" maxlength="30" required /> 
-           </div> 
-          </div> 
-		  <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input id="newpassword" name="newpassword" type="password" class="form-control" placeholder="请输入新密码" maxlength="30" required /> 
-			<span class="help-block text-center">（小提示：密码忘记将无法找回，请慎重设置。）</span>
-           </div> 
-          </div> 
-          <div class="col-md-12"> 
-           <div class="form-group"> 
-            <input type="submit" class="btn btn-primary" value="立即修改" /> 
-           </div> 
-          </div> 
-         </form> 
-        </div> 
-       </div> 
-      </div> 
-     </div> 
-     <div class="modal-footer"> 
-      <button type="button" class="btn btn-default" data-dismiss="modal">关闭窗口</button> 
-     </div> 
-    </div> 
-   </div> 
-  </div>
+ctx2.fillStyle = gradient2;
+ctx2.beginPath();
+ctx2.arc(half, half, half, 0, Math.PI * 2);
+ctx2.fill();
+
+// End cache
+
+function random(min, max) {
+  if (arguments.length < 2) {
+    max = min;
+    min = 0;
+  }
+
+  if (min > max) {
+    var hold = max;
+    max = min;
+    min = hold;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function maxOrbit(x, y) {
+  var max = Math.max(x, y),
+    diameter = Math.round(Math.sqrt(max * max + max * max));
+  return diameter / 2;
+  //星星移动范围，值越大范围越小，
+}
+
+var Star = function() {
+
+  this.orbitRadius = random(maxOrbit(w, h));
+  this.radius = random(60, this.orbitRadius) / 8; 
+  //星星大小
+  this.orbitX = w / 2;
+  this.orbitY = h / 2;
+  this.timePassed = random(0, maxStars);
+  this.speed = random(this.orbitRadius) / 50000; 
+  //星星移动速度
+  this.alpha = random(2, 10) / 10;
+
+  count++;
+  stars[count] = this;
+}
+
+Star.prototype.draw = function() {
+  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+    y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+    twinkle = random(10);
+
+  if (twinkle === 1 && this.alpha > 0) {
+    this.alpha -= 0.05;
+  } else if (twinkle === 2 && this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  ctx.globalAlpha = this.alpha;
+  ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+  this.timePassed += this.speed;
+}
+
+for (var i = 0; i < maxStars; i++) {
+  new Star();
+}
+
+function animation() {
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.globalAlpha = 0.5; //尾巴
+  ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)';
+  ctx.fillRect(0, 0, w, h)
+
+  ctx.globalCompositeOperation = 'lighter';
+  for (var i = 1, l = stars.length; i < l; i++) {
+    stars[i].draw();
+  };
+
+  window.requestAnimationFrame(animation);
+}
+
+animation();
+</script>
+
+	<!-- 音乐代码 -->
+	<audio autoplay>
+	<source src="<?php echo $con6?>" loop="-17" type="audio/mpeg">
+<?php echo $tongji?>
 </body>
 </html>
